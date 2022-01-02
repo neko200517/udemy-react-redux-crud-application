e3d0f72fd022797ae73af94182942b391db8553a
pam190331 <pam130901@gmail.com>
2022-01-02 16:02:01 +0900

    12.more-jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
+// これすらいらない
 import React from 'react';
 
-// 11. JSX
+// 12. もっとJSX
+// class -> className
+// onClick,
+// onChange
+// for -> htmlFor
 
 function App() {
-  // JSX : より直感的にかける
-  return <h1>Hello World!</h1>;
+  const greeting = 'Hello World!';
+  const dom = (
+    // htmlにdivを反映させたくない場合、React.Fragmentを用いればdivを構造に入れなくても済む
+    // ※最近のreactは<div id = "root">に置き換わった模様
+    <React.Fragment>
+      <div className='foo'>{greeting}</div>
 
-  // JSXはこのように書き換えられる
-  // return React.createElement('div', null, 'Hello World!');
+      {/* onClick */}
+      <button
+        onClick={() => {
+          alert('foo');
+        }}
+      >
+        Click Me
+      </button>
+
+      <br />
+
+      {/* htmlFor */}
+      <label htmlFor='bar'>bar</label>
+
+      {/* onChange */}
+      <input
+        type='text'
+        id='bar'
+        onChange={() => {
+          console.log('input change.');
+        }}
+      />
+    </React.Fragment>
+  );
+  return dom;
 }
 
 export default App;
