9ad9ed0bcd30e1b3946340b8b78ed1006b086d97
pam190331 <pam130901@gmail.com>
2022-01-02 17:53:19 +0900

    16.props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 
-function App() {
+// 16. props
+// プロパティを渡す
+
+interface UserType {
+  name: string;
+  age?: number;
+}
+
+const App = () => {
+  const users: UserType[] = [
+    { name: 'taro', age: 11 },
+    { name: 'hanako', age: 11 },
+    { name: 'kumi' },
+  ];
+  const list = users.map((x, i) => {
+    // イテレーションする場合、keyをつけないと警告される
+    return <User key={i} name={x.name} age={x.age}></User>;
+  });
+  return <div>{list}</div>;
+};
+
+const User = (props: UserType) => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      Hi! {props.name} and {props.age} years old.
     </div>
   );
-}
+};
+
+// 型の定義
+User.defaultProps = {
+  age: 1,
+};
 
 export default App;
