8c168c552669a8ba5a5f9436e5958c21dfb6dda8
pam190331 <pam130901@gmail.com>
2022-01-02 15:44:37 +0900

    11.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+// 11. JSX
+
 function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+  // JSX : より直感的にかける
+  return <h1>Hello World!</h1>;
+
+  // JSXはこのように書き換えられる
+  // return React.createElement('div', null, 'Hello World!');
 }
 
 export default App;
