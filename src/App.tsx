e8e936f3771f7df047f16608081e28ea8aa31e05
pam190331 <pam130901@gmail.com>
2022-01-02 16:40:00 +0900

    15.Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
 // これすらいらない
-import React from 'react';
+import React, { Component } from 'react';
 
-// 12. もっとJSX
-// class -> className
-// onClick,
-// onChange
-// for -> htmlFor
+// 15. Component
+// クラスコンポーネント、関数コンポーネントの2種類が存在する
+// ※最近では関数コンポーネントが推奨されている
 
-function App() {
-  const greeting = 'Hello World!';
-  const dom = (
-    // htmlにdivを反映させたくない場合、React.Fragmentを用いればdivを構造に入れなくても済む
-    // ※最近のreactは<div id = "root">に置き換わった模様
-    <React.Fragment>
-      <div className='foo'>{greeting}</div>
+// クラスコンポーネント
+// class App extends Component {
+//   render() {
+//     return <div>Hello!</div>;
+//   }
+// }
 
-      {/* onClick */}
-      <button
-        onClick={() => {
-          alert('foo');
-        }}
-      >
-        Click Me
-      </button>
-
-      <br />
-
-      {/* htmlFor */}
-      <label htmlFor='bar'>bar</label>
-
-      {/* onChange */}
-      <input
-        type='text'
-        id='bar'
-        onChange={() => {
-          console.log('input change.');
-        }}
-      />
-    </React.Fragment>
-  );
-  return dom;
-}
+// 関数コンポーネント
+const App = () => {
+  return <div>Hello!</div>;
+};
 
 export default App;
